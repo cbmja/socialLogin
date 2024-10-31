82d4d0992ad44e4599ac55fc4974ccb0aba7b705
cbmja <cbmja@naver.com>
2024-11-01 01:50:23 +0900

    .

---
+++ src/main/java/com/social/login/sociallogintut/SociallogintutApplication.java
@@ -0,0 +1,13 @@
+package com.social.login.sociallogintut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociallogintutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociallogintutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/login/sociallogintut/SociallogintutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.login.sociallogintut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociallogintutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
